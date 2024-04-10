eb577b66c450f98dfac65d40b3756a9a7b183608
Nick Wydra <njwydra@gmail.com>
2024-04-09 21:53:31 -0500

    update card style and add links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,37 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Nick Wydra',
-  description: 'Digital business card, created by Next',
-}
+  title: "Nick Wydra",
+  description: "Digital business card, created by Next",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nanum+Myeongjo"></link>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato"></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
       </head>
-      <body className={inter.className} style={{ margin: 0 }}>{children}</body>
+      <body className={inter.className} style={{ margin: 0 }}>
+        <main>{children}</main>
+      </body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,24 @@
-import styles from './page.module.css'
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <ul className={styles.circles}>
-        <li></li>
-        <li></li>
-      </ul>
-      <div className={styles.description}>
-        <h1>
-          Nick Wydra
-        </h1>
-        <h2>Software Engineer | Chicago, IL</h2>
-        <a href='mailto:njwydra@gmail.com'>njwydra@gmail.com</a>
+    <div className={styles.description}>
+      <div className={styles.colorBar}></div>
+      <h1>Nick Wydra</h1>
+      <h2>Senior Software Engineer: Tech Lead</h2>
+      <p className={styles.city}>Chicago, IL</p>
+      <div className={styles.links}>
+        <a href="https://linkedin.com/in/nick-wydra" target="_blank">
+          <img height={24} width={24} src="/linkedin.svg" alt="Email" />
+        </a>
+        <a href="https://github.com/njwydra" target="_blank">
+          <img height={24} width={24} src="/github.svg" alt="Email" />
+        </a>
+        <a href="mailto:njwydra@gmail.com">
+          <img height={24} width={24} src="/mail.svg" alt="Email" />
+        </a>
       </div>
-    </main>
-  )
+      <div className={styles.colorBar}></div>
+    </div>
+  );
 }
